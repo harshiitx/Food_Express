9892c0c33d9fcbab401f07e389a3cca865944b0e
Harshit <harshit.bhardwaj@thoughtspot.com>
2025-04-22 16:39:50 +0530

    updated components with accessibility improvements (2025-04-22 11:09:50)

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,9 @@
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 const Footer = () => {
   return (<div id="Footer">
-    <Link className='links' to="/about">About Us</Link>
-    <Link className='links' to="/contact">Contact Us</Link>
-    <Link className='links' to="/feedback">Feedback</Link>
+    <Link className='links' to="/about" aria-label="About Us">About Us</Link>
+    <Link className='links' to="/contact" aria-label="Contact Us">Contact Us</Link>
+    <Link className='links' to="/feedback" aria-label="Feedback">Feedback</Link>
   </div>)
 };
 export default Footer;
